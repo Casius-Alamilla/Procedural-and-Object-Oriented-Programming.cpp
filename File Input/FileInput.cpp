2e83de2cfb8c91f03e64969c903f549c1e1ce191
Casius <131908260+Casius-Alamilla@users.noreply.github.com>
2026-02-04 18:17:16 -0600

    Document file reading functionality in FileInput.cpp
    Add comments outlining the functionality for reading words from a file and displaying them.

--- File Input/FileInput.cpp
+++ File Input/FileInput.cpp
@@ -1,3 +1,15 @@
+/*
+1. Open a file called “words.txt” for input
+
+2. Read in each word from the file. Each word is separated by a space. The file will have at
+least one word. The program will not know how many words are in the file.
+
+    a. As the program reads in each word, it will output the words to the console
+        (separated by a space).
+    b. On a separate line, output the number of words that were found in the file.
+*/
+
+
 #include <iostream>
 #include <fstream>
 #include <string>
